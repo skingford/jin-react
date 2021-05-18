343463f946c070a6ec470d2d57afd2a504dab1f0
SteveKingFord <332312568@qq.com>
2021-05-18 10:54:49 +0800

    test: about

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,14 +1,14 @@
 /*
  * @Author: kingford
  * @Date: 2021-05-17 14:23:01
- * @LastEditTime: 2021-05-17 14:27:23
+ * @LastEditTime: 2021-05-18 10:54:27
  */
 import { Link } from 'react-router-dom';
 
 const About:React.FC = ()=>{
   return (
      <div>
-       <h1>About</h1>
+       <h1>About hook</h1>
        <nav>
         <Link to="/">Home</Link> |{' '}
         <Link to="about">About</Link>
